c9bc0c07d4d611475efc7c1061b923d9b5f4e28f
Jarod <jarodr@decho.com>
2009-05-06 18:37:50 -0700

    add comments

--- 1/problem_one.rb
+++ 1/problem_one.rb
@@ -1,3 +1,4 @@
+# Add all the natural numbers below one thousand that are multiples of 3 or 5.
 result = 0
 (1..999).each do |n|
   if ((n % 3) == 0) || ((n % 5) == 0)
--- 2/problem_two.rb
+++ 2/problem_two.rb
@@ -1,3 +1,4 @@
+# Find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed four million.
 result = 0; curr = 0; succ = 1;
 while curr < 4000000
   curr, succ = succ, succ + curr
