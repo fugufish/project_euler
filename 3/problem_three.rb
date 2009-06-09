a4de10563b5f5f3f4f8defba6689fbae22bac3b7
Jarod Reid <administrator@diapaswap.local>
2009-06-09 13:20:06 -0600

    problem 3

---
+++ 3/problem_three.rb
@@ -0,0 +1,21 @@
+class Fixnum
+  
+  def prime?
+    return true unless ('1' * self) =~ /^1$|^(11+?)\1+$/
+    false
+  end
+  
+end
+
+require 'benchmark'
+
+puts Benchmark.measure {
+  
+  Math.sqrt(600851475143).ceil.downto(2) do |i|
+    if 600851475143 % i == 0 && i.prime?
+      puts i
+      break
+    end
+  end
+  
+}
