b5f9cf8127beabe7060119324000b1c2963b5325
Jarod Reid <administrator@jarod.pmamediagroup.net>
2009-06-10 12:36:56 -0600

    4 to return one value

--- 4/problem_four.rb
+++ 4/problem_four.rb
@@ -2,13 +2,14 @@
 
 max = 999 * 999
 brk = 0
+ret = ''
 max.downto(1) do |i|
   if i.to_s.reverse == i.to_s
     999.downto(100) do |j|
       if i % j == 0
         k = i / j
         if k.to_s.length == 3
-          puts i
+          ret = i
           brk = 1
         end
       end
@@ -16,3 +17,4 @@ max.downto(1) do |i|
   end
   break if brk == 1
 end
+puts ret
