d5078c73c48130cb12a854f1210748b08f694329
Jarod Reid <administrator@jarod.pmamediagroup.net>
2009-06-10 12:32:41 -0600

    added 4

---
+++ 4/problem_four.rb
@@ -0,0 +1,18 @@
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+max = 999 * 999
+brk = 0
+max.downto(1) do |i|
+  if i.to_s.reverse == i.to_s
+    999.downto(100) do |j|
+      if i % j == 0
+        k = i / j
+        if k.to_s.length == 3
+          puts i
+          brk = 1
+        end
+      end
+    end
+  end
+  break if brk == 1
+end
