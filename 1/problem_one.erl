f998609473d03f4f68f310dec041cb128b99f280
Jarod Reid <jarod@solidalchemy.com>
2009-09-09 07:29:28 -0600

    problem two in erlang

--- 1/problem_one.erl
+++ 1/problem_one.erl
@@ -2,5 +2,4 @@
 -module(problem_one).
 -export([calculate/1]).
 
-calculate(Number) ->
-	lists:sum(lists:filter(fun(X) -> (X rem 3 == 0) orelse (X rem 5 == 0) end, lists:seq(1, (Number - 1)))).
+calculate(Number) -> lists:sum(lists:filter(fun(X) -> (X rem 3 == 0) orelse (X rem 5 == 0) end, lists:seq(1, (Number - 1)))).
