78524692e5a5c7081c12d7ef00c9daadd4248db5
Jarod <jarodr@pmamediagroup.com>
2010-07-29 13:45:32 -0600

    beat chendo

--- 10/problem_ten.rb
+++ 10/problem_ten.rb
@@ -1,21 +1,6 @@
 require '../lib/sieve'
-# class Integer
-#   
-#   def prime?
-#     return true if self == 2
-#     return false if (self & 1) == 0
-#     square = Math.sqrt(self).round + 1
-#     i = 1
-#     while i <= square
-#       i+= 2
-#       return false if (self % i) == 0
-#     end
-#     true
-#   end
-#   
-# end
 
-# numbers = (2..2000000)
-# 
-# numbers = numbers.select { |n| n.prime? }
-puts 2000000.primes.inject { |result, prime| result + prime }
+x = 0
+a = 2000000.primes
+a.each{ |i| x+= i }
+puts x
